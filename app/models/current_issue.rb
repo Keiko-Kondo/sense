695aa7f865bab5a12166b3fa6f2f5ab2ac920122
keiko kondo <keiko.ayaka@icloud.com>
2021-01-20 23:02:49 +0900

    current_issue作成

--- app/models/current_issue.rb
+++ app/models/current_issue.rb
@@ -5,4 +5,8 @@ class CurrentIssue < ApplicationRecord
   # validates :address, presence: true
   # validates :latitude, presence: true
   # validates :longitude, presence: true
+
+
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
 end
